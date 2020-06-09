f9f188db8edc2acc2624aaaadb632eefb9d73fea
songruining <songruining@acmoba.net>
2020-06-09 14:09:44 +0800

    add Proto, Upval, LocalVal

--- src/compiler.rs
+++ src/compiler.rs
@@ -1,11 +1,42 @@
-use crate::ast::Expr;
 use crate::ast::*;
 use crate::ast_walker::AstVisitor;
-// use crate::opcodes::OpCode;
+use crate::opcodes::Instruction;
 use crate::types::{FloatType, IntType};
+
+enum Const {
+    Int(IntType),
+    Float(FloatType),
+    Str(String),
+}
+pub struct LocalVal {}
+pub struct UpVal {}
+
+pub struct Proto {
+    code: Vec<Instruction>,
+    consts: Vec<Const>,
+    local_vars: Vec<LocalVal>,
+    up_vars: Vec<UpVal>,
+    protos: Vec<Proto>,
+}
+
+impl Proto {
+    pub fn new() -> Proto {
+        Proto {
+            code: Vec::new(),
+            consts: Vec::new(),
+            local_vars: Vec::new(),
+            up_vars: Vec::new(),
+            protos: Vec::new(),
+        }
+    }
+}
 
 pub struct Compiler {}
 
-impl AstVisitor for Compiler {
-
+impl Compiler {
+    pub fn run(&mut self, block: &Block) -> Proto {
+        Proto::new()
+    }
 }
+
+impl AstVisitor for Compiler {}
