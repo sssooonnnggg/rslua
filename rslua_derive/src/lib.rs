2cdd798ea17b7c462741f73f50ad9f96f5bb719d
MSI\songrn <sssooonnnggg111@gmail.com>
2023-03-27 22:11:16 +0800

    fix: fixed compile issue

--- rslua_derive/src/lib.rs
+++ rslua_derive/src/lib.rs
@@ -1,13 +1,13 @@
-use proc_macro::TokenStream;
-use quote::quote;
-use syn;
+// use proc_macro::TokenStream;
+// use quote::quote;
+// use syn;
 
-#[proc_macro_derive(HelloMacro)]
-pub fn hello_macro_derive(input: TokenStream) -> TokenStream {
-    // Construct a representation of Rust code as a syntax tree
-    // that we can manipulate
-    let ast = syn::parse(input).unwrap();
+// #[proc_macro_derive(HelloMacro)]
+// pub fn hello_macro_derive(input: TokenStream) -> TokenStream {
+//     // Construct a representation of Rust code as a syntax tree
+//     // that we can manipulate
+//     let ast = syn::parse(input).unwrap();
 
-    // Build the trait implementation
-    impl_hello_macro(&ast)
-}
+//     // Build the trait implementation
+//     impl_hello_macro(&ast)
+// }
